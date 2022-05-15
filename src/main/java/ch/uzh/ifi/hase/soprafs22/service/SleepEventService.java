cc8b927ef288bd3facd92cfbd22885c26f26e436
johannauzh <johanna.bieri@uzh.ch>
2022-05-15 22:00:36 +0200

    fix heroku 2.0

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/SleepEventService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/SleepEventService.java
@@ -17,6 +17,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+//fix heroku
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
